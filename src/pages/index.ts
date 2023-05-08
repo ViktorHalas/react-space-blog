8eee33a1a80dabb1d820ffdc3d5bd296e8d9edaf
ViktorHalas <99_vita@mail.ru>
2023-05-08 22:15:07 +0300

    feat(pages): add DetailsPage, HomePage, SearchPage, SingInPage, SingUpPage

---
+++ src/pages/index.ts
@@ -0,0 +1,7 @@
+import { DetailsPage } from "./DetailsPage";
+import { HomePage } from "./HomePage";
+import { SearchPage } from "./SearchPage";
+import { SignInPage } from "./SignInPage";
+import { SignUpPage } from "./SignUpPage";
+
+export { DetailsPage, HomePage, SearchPage, SignInPage, SignUpPage};

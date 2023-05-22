5f4c9123ac720edcb15155dfb3ec0fcd2fe9baec
ViktorHalas <99_vita@mail.ru>
2023-05-22 08:42:33 +0300

    feat(templates): add theme

--- src/templates/MainTemplate.tsx
+++ src/templates/MainTemplate.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
+import { useAppSelector } from "store/hooks/hooks";
+import { getTheme } from "store/selectors/themeSelector";
 
 export const MainTemplate = () => {
+  const { theme } = useAppSelector(getTheme);
+  useEffect(() => document.documentElement.setAttribute("theme", theme), [theme]);
   return (
     <div>
       <Outlet />

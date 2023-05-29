ceba184ee5151bea47c26b3ed1e822c233fe78de
ViktorHalas <99_vita@mail.ru>
2023-05-29 09:47:03 +0300

    feat(types): add types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,11 +1,11 @@
-export interface NewsInfo {
+export interface BlogInfo {
     id:	number;
     featured?: boolean;
     default: false;
     title: string;
     url: string;
     imageUrl: string;
-    newsSite: string;
+    blogSite: string;
     summary: string;
     publishedAt: string;
     launches: Launch[];
@@ -22,3 +22,27 @@ interface Event {
 };
 
 export type Theme = "dark" | "light";
+
+export interface TabsBlogInfo {
+    id: string;
+    label: string;
+};
+
+export interface DaysInfo {
+    id: string;
+    label: string;
+};
+
+export interface SelectOption {
+    value: string;
+    label: string;
+}
+
+export interface RequestOption {
+    blog?:string | undefined;
+    id?:string | undefined ;
+    sort?:string;
+    limit?:string;
+    start?:string;
+    search?:string;
+}

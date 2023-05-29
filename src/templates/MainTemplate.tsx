06d18f74b0ee61d3dc66e35d0ac4aa0df8448c83
ViktorHalas <99_vita@mail.ru>
2023-05-29 09:44:48 +0300

    refactor(templates): add container in MainTemplate

--- src/templates/MainTemplate.tsx
+++ src/templates/MainTemplate.tsx
@@ -1,14 +1,21 @@
+import { Footer } from "components";
+import { Header } from "components/Header/Header";
 import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import { useAppSelector } from "store/hooks/hooks";
 import { getTheme } from "store/selectors/themeSelector";
+import { MainContainer, Wrapper } from "./styles";
 
 export const MainTemplate = () => {
   const { theme } = useAppSelector(getTheme);
   useEffect(() => document.documentElement.setAttribute("theme", theme), [theme]);
   return (
-    <div>
-      <Outlet />
-    </div>
+    <Wrapper>
+      <Header />
+      <MainContainer>
+        <Outlet />
+      </MainContainer>
+      <Footer />
+    </Wrapper>
   );
 };

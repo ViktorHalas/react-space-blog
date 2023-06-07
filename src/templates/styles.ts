b5e2733fe62eed405fa9dcb46b50d6fc7c198603
ViktorHalas <99_vita@mail.ru>
2023-06-07 19:10:32 +0300

    fear(templates):add styles

--- src/templates/styles.ts
+++ src/templates/styles.ts
@@ -1,15 +1,23 @@
 import styled from "styled-components";
+import { Media } from "ui";
 
 const Wrapper = styled.div`
-    min-height: 100vh;
-    display: grid;
-    grid-template-rows: auto 1fr auto;
+/* display: grid;
+grid-template-rows: auto 1fr auto; 
+min-height: 100vh;
+width: 100%; */
 `;
 const MainContainer = styled.div`
-    margin: 0 auto;
-    width: 100%;
-    height: 100%;
-    max-width: 1120px;
+  margin: 0 auto 72px auto;
+  max-width: 1120px;
+  width: 100%;
+  height: 100%;
+  ${Media.XL}{
+      padding: 0px 40px;
+  };
+  ${Media.SM}{
+      padding: 0px 24px;
+  };
 `;
 
 export {Wrapper, MainContainer};

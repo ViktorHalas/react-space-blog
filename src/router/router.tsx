a56d15c8da5026effcee1335eaab7f68a2801d94
ViktorHalas <99_vita@mail.ru>
2023-06-13 07:36:34 +0300

    build(app): download cloud firestore

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,20 +1,28 @@
-import { AccountPage, DetailsPage, HomePage, SearchPage, SignInPage, SignUpPage } from "pages";
+import {
+  AccountPage,
+  DetailsPage,
+  FavoritesPage,
+  HomePage,
+  SearchPage,
+  SignInPage,
+  SignUpPage,
+} from "pages";
 import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import { ROUTE } from "./routes";
-
 import { MainTemplate } from "templates";
-import { PasswordResetPage } from "pages/PasswordResetPage/PasswordResetPage";
+import { PasswordResetPage } from "pages";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path={ROUTE.HOME} element={<MainTemplate />}>
-      <Route index path={ROUTE.HOME} element={<HomePage />} />
+      <Route index element={<HomePage />} />
       <Route path={ROUTE.SEARCH} element={<SearchPage />} />
       <Route path={ROUTE.DETAILS} element={<DetailsPage />} />
       <Route path={ROUTE.SIGN_IN} element={<SignInPage />} />
       <Route path={ROUTE.SIGN_UP} element={<SignUpPage />} />
       <Route path={ROUTE.RESET_PASSWORD} element={<PasswordResetPage />} />
       <Route path={ROUTE.ACCOUNT} element={<AccountPage />} />
+      <Route path={ROUTE.FAVORITES} element={<FavoritesPage />} />
     </Route>,
   ),
 );
